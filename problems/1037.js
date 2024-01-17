c0d8db3a1537593393bcb3e650c7014f4fdbc6b1
brunnaserafina <brunnaserafina@gmail.com>
2024-01-16 21:11:49 -0300

    fix: exercise 1037

--- problems/1037.js
+++ problems/1037.js
@@ -1,6 +1,6 @@
 //Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
 
-function intervalo() {
+function intervalo(lines) {
   const value = lines[0];
 
   if (value >= 0 && value <= 25) {
@@ -17,4 +17,4 @@ function intervalo() {
 }
 
 //entrada:
-formulaBaskara(["25.00"]);
+intervalo(["25.00"]);

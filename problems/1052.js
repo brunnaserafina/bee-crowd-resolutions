9beed0d91b7156ab70e614a27006ca81ecda04b8
brunnaserafina <brunnaserafina@gmail.com>
2024-08-24 14:00:18 -0300

    feat: Exercise 1052

---
+++ problems/1052.js
@@ -0,0 +1,29 @@
+// Leia um valor inteiro entre 1 e 12, inclusive. 
+// Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+// Entrada: A entrada contém um único valor inteiro.
+// Saída: Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula.
+
+function retornaMes(lines) {
+  const numeroMes = Number(lines[0]);
+
+  const meses = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+  };
+
+  console.log(meses[numeroMes]);
+}
+
+// Exemplo de entrada:
+retornaMes(["7"]);
